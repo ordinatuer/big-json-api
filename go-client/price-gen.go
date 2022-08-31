b3f3c7415655f45ba3f6a6aba4223a3d64a48ed7
Ordinatuer <ordinatuer@yandex.ru>
2022-08-31 22:04:37 +0300

    Генератор запросов на Go + профайлер запросов в Symfony

--- go-client/price-gen.go
+++ go-client/price-gen.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"os"
 	"strconv"
 )
 
@@ -31,11 +32,14 @@ func main() {
 	var price_selling float64 = 1200
 	var price_discount float64 = 1100
 
-	for i := 1; i <= 1000; i++ {
+	var count_products int = 10
+	var count_regions int = 20
+
+	for i := 1; i <= count_products; i++ {
 		product_id++
 
 		region_price := map[string]interface{}{}
-		for r := 1; r <= 20; r++ {
+		for r := 1; r <= count_regions; r++ {
 			price_purchase = inc(price_purchase)
 			price_selling = inc(price_selling)
 			price_discount = inc(price_discount)
@@ -57,15 +61,33 @@ func main() {
 		"data": {string(b)},
 	}
 
-	resp, err := http.PostForm(api_url, formData)
+	writeInFile(string(b), "request.json")
+
+	response, err := http.PostForm(api_url, formData)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	fmt.Println(resp)
+	defer response.Body.Close()
+
+	var res map[string]interface{}
+	json.NewDecoder(response.Body).Decode(&res)
+
+	fmt.Println(response.StatusCode)
 }
 
 func inc(price float64) float64 {
 	var i float64 = 1
 	return price + i
+}
+
+func writeInFile(str string, fileName string) {
+	file, err := os.Create(fileName)
+	if err != nil {
+		fmt.Println("File create error ", err)
+	}
+
+	defer file.Close()
+
+	file.WriteString(str)
 }
